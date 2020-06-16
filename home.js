591cf0af5739fb43c7eb96e74eb96be72bbabe66
mecjed12 <exopas@outlook.de>
2020-06-16 15:29:21 +0200

    zoowebsite

--- home.js
+++ home.js
@@ -1,19 +1,43 @@
-var slideIndex = 1;
+// var slideIndex = 1;
 
-function showSlides(n) {
-    console.log("HWEY")
-    var i ;
-    var slides = document.getElementsByClassName("Slides-fade");
-    var lines = document.getElementsByClassName("line");
-    if (n > slides.lenght) {slideIndex = 1}
-    if (n < 1) {slidesIndex = slide.lenght}
-    for (i = 0 ; i < slides.length; i++) {
-        slides [1].style.display = "none";
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+
+
+// function showSlides(n) {
+//     console.log("HWEY")
+//     var i ;
+//     var slides = document.getElementsByClassName("Slides-fade");
+//     var lines = document.getElementsByClassName("lines");
+//     if (n > slides.lenght) {slideIndex = 1}
+//     if (n < 1) {slidesIndex = slide.lenght}
+//     for (i = 0 ; i < slides.length; i++) {
+//         slides [1].style.display = "none";
+//     }
+
+// }
+// for (i = 0; i < lines.lenght ; i++) {
+//     lines[i].className = line[i].className.replace("active","");
+// }
+// slides[slideIndex-1].style.display = "block";
+// lines[slideIndex-1].className += "active";
+
+
+function changeImage(slideNumber) {
+    var imageContainer = document.getElementById("image-container")
+    switch(slideNumber) {
+        case 1: {
+            imageContainer.style.backgroundImage = 'url(img_2228.jpg)'
+            break;
+        }
+        case 2: {
+            imageContainer.style.backgroundImage = 'url(img_1491_0.jpg)'
+            break;
+        }
+        case 3: {
+            imageContainer.style.backgroundImage ='url(janko-ferlic-ypS9j3UzqLk-unsplash.jpg)'
+            break;
+        }
     }
-
 }
-for (i = 0; i < lines.lenght ; i++) {
-    lines[i].className = line[i].className.replace("active","");
-}
-slides[slideIndex-1].style.display = "block";
-lines[slideIndex-1].className += "active";
